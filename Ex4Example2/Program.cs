98ce7f5373981b0f0b7c2646bf4fbefaf8c83efc
KharisovR <alkatrazrudi@gmail.com>
2022-07-10 18:51:13 +0300

    Оформили блок схему и решение 4 задачи с участием пользователя

---
+++ Ex4Example2/Program.cs
@@ -0,0 +1,43 @@
+﻿//4. Найти максимальное из трех чисел
+// Я вижу решение данной задачи следующим образом: Имеется три случайных числа, программа должна найти наибольшее
+Console.WriteLine("Введите число от 1 до 7");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 1)
+{
+    Console.WriteLine(number);
+    Console.WriteLine("Monday");
+}
+if (number == 2)
+{
+    Console.WriteLine(number);
+    Console.WriteLine("Tuesday");
+}
+if (number == 3)
+{
+    Console.WriteLine(number);
+    Console.WriteLine("Wensday");
+}
+if (number == 4)
+{
+    Console.WriteLine(number);
+    Console.WriteLine("Thursday");
+}
+if (number == 5)
+{
+    Console.WriteLine(number);
+    Console.WriteLine("Friday");
+}
+if (number == 6)
+{
+    Console.WriteLine(number);
+    Console.WriteLine("Saturday");
+}
+if (number == 7)
+{
+    Console.WriteLine(number);
+    Console.WriteLine("Sunday");
+}
+if (number > 7)
+{
+    Console.WriteLine("В неделе только 7 дней");
+}
