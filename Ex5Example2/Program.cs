ee105f7f5ef376d6bd11a2b1553cf6b1b233e717
KharisovR <alkatrazrudi@gmail.com>
2022-07-10 20:48:39 +0300

    Решили 5 задачу с участием пользователя

---
+++ Ex5Example2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 4. Найти максимальное из трех чисел
+//Я вижу решение данной задачи следующим образом: Имеем 3 натуральных числа, нужно найти наибольшее из них
+
+Console.WriteLine("Введите первое число");
+double a = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+double b = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+double  c = double.Parse(Console.ReadLine()!);
+double max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("Максимальное число равно: " +max);
