cb2fd9cd6fc1e32e185e4434f3256dd49e01158f
KharisovR <alkatrazrudi@gmail.com>
2022-07-10 16:07:01 +0300

    Написали еще один  вариант решения второй задачи

---
+++ Ex2Example2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите первое число");
+double numberA = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+double numberB = double.Parse(Console.ReadLine()!);
+
+if(numberA / numberB == numberB)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
