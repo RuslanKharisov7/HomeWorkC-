db3eda2ad719de394ae96b8583dd6eeb10187764
KharisovR <alkatrazrudi@gmail.com>
2022-07-10 16:52:04 +0300

    Создали блок схему и написали решение третей задачи

---
+++ Ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿//2. Даны два числа. Показать большее и меньшее число
+
+double numberA = new Random().Next();
+double numberB = new Random().Next();
+Console.WriteLine("Первое число");
+Console.WriteLine(numberA);
+Console.WriteLine("Второе число");
+Console.WriteLine(numberB);
+
+if (numberA>numberB)
+{
+    Console.WriteLine("Максимально число");
+    Console.WriteLine(numberA);
+    Console.WriteLine("Минимальное число");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.WriteLine("Максимально число");
+    Console.WriteLine(numberB);
+    Console.WriteLine("Минимальное число");
+    Console.WriteLine(numberA);
+}
