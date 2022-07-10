3c57562ade50b7ac5bd4448a2f068b38037c1cb6
KharisovR <alkatrazrudi@gmail.com>
2022-07-10 17:11:44 +0300

    Решили 3 задачу, с участием пользователя

---
+++ Ex3Example2/Program.cs
@@ -0,0 +1,21 @@
+﻿//2. Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите первое число");
+double numberA = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+double numberB = double.Parse(Console.ReadLine()!);
+
+if (numberA>numberB)
+{
+    Console.WriteLine("Максимально число");
+    Console.WriteLine(numberA);
+    Console.WriteLine("Минимальное число");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.WriteLine("Максимально число");
+    Console.WriteLine(numberB);
+    Console.WriteLine("Минимальное число");
+    Console.WriteLine(numberA);
+}
