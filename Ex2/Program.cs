0060a35a8e2278d3dac8d0682419835fadd1a7b0
KharisovR <alkatrazrudi@gmail.com>
2022-07-10 15:32:51 +0300

    Создали блок схему и записали решегие второго задания

---
+++ Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿//1. По двум заданным числам проверять является ли первое квадратом второго
+// Я вижу решение данной задачи следующим образом: Заданы два числа и программа должна проверить является ли первое число
+// квадратом второго
+
+double numberA = new Random().Next(-100, 1000);
+double numberB = new Random().Next(-100, 1000);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+if (numberA/numberB == numberB)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
+
