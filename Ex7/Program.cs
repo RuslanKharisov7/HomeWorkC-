35dc6f1047517da37553d17b1f34ac7c8707daf9
KharisovR <alkatrazrudi@gmail.com>
2022-07-11 00:17:40 +0300

    Решили 7 задачу

---
+++ Ex7/Program.cs
@@ -0,0 +1,11 @@
+﻿// 7. Показать числа от -N до N
+
+Console.WriteLine("Введите число");
+double N = double.Parse(Console.ReadLine()!);
+double M = -N;
+
+while (M<N)
+{
+  Console.WriteLine(M);
+  M = M + 1;
+}
