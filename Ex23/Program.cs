0f2fa0ec426781c20041292f1b48c6a105705c43
KharisovR <alkatrazrudi@gmail.com>
2022-07-19 22:13:36 +0300

    Решили задачу 23

---
+++ Ex23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите целое положительное число");
+double N = double.Parse(Console.ReadLine()!);
+double M = 1;
+while(M<=N)
+{
+    Console.WriteLine(Math.Pow(M,3));
+       M++;
+}
