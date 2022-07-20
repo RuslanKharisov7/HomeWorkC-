73ce609578d7f72c8d7afba353c586ce3b4cc376
KharisovR <alkatrazrudi@gmail.com>
2022-07-20 23:54:01 +0300

    Решил задачу 11 вторым способом

---
+++ Ex11.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// // 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.WriteLine("Введите двух значное число от 10 до 99");
+int number = int.Parse(Console.ReadLine()!);
+int x = number / 10;
+int y = number % 10;
+if (x>y)
+{
+    Console.WriteLine($"Наибольшая цифра введеного числа : {x}");
+    Console.WriteLine($"Наименьшая цифра введеного числа : {y}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра введеного числа : {y}");
+    Console.WriteLine($"Наименьшая цифра введеного числа : {x}");
+}
+
