ec5c4ea9f99d582c560bc7e93cd0d72e522635ff
KharisovR <alkatrazrudi@gmail.com>
2022-07-21 22:15:58 +0300

    Решил 12 задачу с участием пользователя

---
+++ Ex12.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число от 100 до 999");
+int num = int.Parse(Console.ReadLine()!);
+int x = (num / 10) % 10;
+if (num > 999 || num < 100)
+{
+    Console.WriteLine("Введенное число не входит в указанный диапозон");
+}
+else 
+{
+    Console.WriteLine($"Вторая цифра введеного числа : {x}");
+}
