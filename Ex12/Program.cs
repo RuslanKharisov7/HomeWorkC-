5e389b87872a9d57394f8e2f09f3c4f818695699
KharisovR <alkatrazrudi@gmail.com>
2022-07-21 22:04:24 +0300

    Решил 12 задачу

---
+++ Ex12/Program.cs
@@ -0,0 +1,7 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число: {number}");
+int x = (number / 10) % 10;
+Console.WriteLine($"Вторая цифра введеного числа : {x}");
+
