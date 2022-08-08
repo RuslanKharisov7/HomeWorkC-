1a0282561b4778775a00ff37ccfd3e767ccacb0a
KharisovR <alkatrazrudi@gmail.com>
2022-08-09 01:48:56 +0300

    Написал решение 29 задачи

---
+++ Ex29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите метод(ы), которай задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArrray(int[] newArray)
+{
+    int length = newArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        newArray[index] = new Random().Next();
+        index++;
+    }
+}   
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+    Console.WriteLine(array[position]);
+    position++; 
+    }
+}
+int[] array = new int[8];
+FillArrray(array);
+PrintArray(array);
+Console.WriteLine();
