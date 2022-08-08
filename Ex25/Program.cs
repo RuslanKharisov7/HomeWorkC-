5bf681429b5384f859fa2fb58923a11dabd59067
KharisovR <alkatrazrudi@gmail.com>
2022-08-09 00:58:14 +0300

    Написал решение задачи 25

---
+++ Ex25/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("ВВедите число A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine("A в степени B равно:" +  Math.Pow(A, B));
+
+
